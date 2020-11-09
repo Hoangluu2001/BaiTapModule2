9f5f41ce45b3c159f51d79a49e07ffddf40e6131
hoangluu2001 <hoangthehoaluu2gmail.com>
2020-11-09 08:56:03 +0700

    up

--- BaiTapbuoi2/bai1.php
+++ BaiTapbuoi2/bai1.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<meta http-equiv="Content-Type" content="text/html; chatset=utf-8" />
+<meta http-equiv="Content-Type" content="text/html; chatset=utf-8"/>
 <head>
     <title> truyen vao 1 tinh tong tuyen vao 2 la sap xep</title>
 <body>
@@ -8,11 +8,11 @@
     <input type="text" name="nhap" placeholder="nhap so">
     <input type="submit" value="check">
     <?php
-    $cars = array(6,8,10,5,4,13,18);
+    $cars = array(6, 8, 10, 5, 4, 13, 18);
     $a = 1;
     $b = 2;
     if ($a == $_POST["nhap"]) {
-        echo "<br><br> sum = " .array_sum($cars);
+        echo "<br><br> sum = " . array_sum($cars);
     }
     if ($b == $_POST["nhap"]) {
         sort($cars);
